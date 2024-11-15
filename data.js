1f9e50a4badd43ac762001600b9da51822d77cd9
Jalen Battle <jalenbattle78@gmail.com>
2024-11-15 14:11:49 -0500

    added the link thingy

---
+++ data.js
@@ -0,0 +1,14 @@
+const links = [
+    {
+        folder: 0,
+        display_name: "coolKitty",
+        desc: "Click the link to find out",
+        href: "https://youtu.be/2yJgwwDcgV8?si=FreU5hIuHl71UEgU"
+    },
+    {
+        folder: "ex",
+        display_name: "example",
+        desc: "example",
+        href: "example.com"
+    },
+]
